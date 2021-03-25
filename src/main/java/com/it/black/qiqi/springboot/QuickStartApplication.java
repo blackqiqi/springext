a7f086cbe15c9da6703c495dec00fed02e48fc56
blackqiqi <blackqiqi@192.168.0.105>
2021-03-25 22:37:32 +0800

    init quickstart project

---
+++ src/main/java/com/it/black/qiqi/springboot/QuickStartApplication.java
@@ -0,0 +1,15 @@
+package com.it.black.qiqi.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 应用启动类
+ *
+ */
+@SpringBootApplication
+public class QuickStartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuickStartApplication.class, args);
+    }
+}
---
+++ src/main/java/com/it/black/qiqi/springboot/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.it.black.qiqi.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Spring Boot Hello案例
+ *
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @ResponseBody
+    public String sayHello() {
+        return "Hello，Spring Boot！";
+    }
+}
